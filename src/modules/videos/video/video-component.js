d0d9d64ec561b96f448bc0d788e9a089e6db5511
Chris <5120660+poppahorse@users.noreply.github.com>
2020-01-26 18:31:57 +0000

    docs: add docs to video-component, tidy, etc

--- src/modules/videos/video/video-component.js
+++ src/modules/videos/video/video-component.js
@@ -3,121 +3,168 @@ import './video.css';
 import YouTube from 'react-youtube';
 import TrackRange from '../../track/track-range';
 
-// TODO - Need to make this a class based on https://stackoverflow.com/questions/36097965/when-to-use-es6-class-based-react-components-vs-functional-es6-react-components
-// https://github.com/tjallingt/react-youtube/blob/master/example/example.js
-
-
-
-const videoIdA = 'WGoDaYjdfSg';
-const videoIdB = 'tAGnKpE4NCI';
+// youtube video options
 const opts = {
-    height: '100%',
-    width: '100%',
-    playerVars: { // https://developers.google.com/youtube/player_parameters
-      autoplay: 0,
-      controls: 0
-    }
-  };
+	height: '100%',
+	width: '100%',
+	playerVars: {
+		// https://developers.google.com/youtube/player_parameters
+		autoplay: 0,
+		controls: 0,
+	},
+};
 
+/**
+ * Video interface, showing a playing video, controlling with a range slider, and saving loops etc
+ *
+ * @class VideoComponent
+ * @extends {React.Component}
+ */
 class VideoComponent extends React.Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        videoId: props.match.params.id,
-        player: null,
-        progress: 0,
-        duration: 100,
-        range: [0, 100]
-      };
-  
-      this.onReady = this.onReady.bind(this);
-      this.onChangeVideo = this.onChangeVideo.bind(this);
-      this.onPlayVideo = this.onPlayVideo.bind(this);
-      this.onPauseVideo = this.onPauseVideo.bind(this);
-      this.setSpeed = this.setSpeed.bind(this);
-      this.videoTimerProgress = this.videoTimerProgress.bind(this);
-      this.handler = this.handler.bind(this)
-    }
+	/**
+	 *Creates an instance of VideoComponent.
+	 * @param {*} props
+	 * @memberof VideoComponent
+	 */
+	constructor(props) {
+		super(props);
 
-    videoTimerProgress() {
-        this.setState({
-            progress: (100 / (this.state.player.getDuration()) * this.state.player.getCurrentTime()),
-          });
+		this.state = {
+			videoId: props.match.params.id,
+			player: null,
+			progress: 0,
+			duration: 100,
+			range: [0, 100],
+		};
 
-          if(this.state.player.getCurrentTime() >= this.state.range[1]) {
-            this.state.player.seekTo(this.state.range[0]);
-          }
-          //console.log(this.state);
-    }
-  
-    onReady(event) {
-      console.log(`YouTube Player object for videoId: "${this.state.videoId}" has been saved to state.`); // eslint-disable-line
-      this.setState({
-        player: event.target,
-        progress: 0,
-        duration: event.target.getDuration()
-      });
-      
+		this.onReady = this.onReady.bind(this);
+		this.playVideo = this.playVideo.bind(this);
+		this.pauseVideo = this.pauseVideo.bind(this);
+		this.setSpeed = this.setSpeed.bind(this);
+		this.videoTimerProgress = this.videoTimerProgress.bind(this);
+		this.handler = this.handler.bind(this);
 
-        let videoTimer = setInterval(this.videoTimerProgress, 100);
-    }
-  
-    setSpeed() {
-      this.state.player.setPlaybackRate(0.5);
-    }
-  
-    onPlayVideo() {
-      this.state.player.playVideo();
-      //player.getCurrentTime()
-      //player.getDuration()
-      //percent = player.getDuration()
-      console.log(this.state.player.getDuration());
+		this.videoTimer = null;
+	}
 
-    }
-  
-    onPauseVideo() {
-      this.state.player.pauseVideo();
-    }
-  
-    onChangeVideo() {
-      this.setState({
-        videoId: this.state.videoId === videoIdA ? videoIdB : videoIdA,
-      });
-    }
+	/**
+	 * Handles repeatable operations for updating progress etc
+	 *
+	 * @memberof VideoComponent
+	 */
+	videoTimerProgress() {
+		// sets state with progress
+		this.setState({
+			progress:
+				(100 / this.state.player.getDuration()) *
+				this.state.player.getCurrentTime(),
+		});
 
-    handler(event) {
-        console.log('handler', event);
-        this.setState({
-            range: event
-        });
-        this.state.player.seekTo(event[0]);
-    }
-  
-    render() {
-      return (
-        <div class="wrapper">
-            <section class="main">
-                    <YouTube videoId={this.state.videoId} onReady={this.onReady} opts={opts} containerClassName='video' />
-                <div class="controls">
-                    <div class="track-wrapper">
-                        <div class="track-progress" style={{width: this.state.progress + '%'}}></div>
-                    </div>
-                    <TrackRange handler={this.handler} duration={ this.state.duration } />
-                    <div class="buttons">
-                        <button onClick={this.onPlayVideo}>Play</button>
-                        <button onClick={this.onPauseVideo}>Pause</button>
-                        <button onClick={this.onChangeVideo}>Change Video</button>
-                        <button onClick={this.setSpeed}>Slow</button>
-                    </div>
-                </div>
-            </section>
-            <section class="nav">
-                YOOO
-            </section>
-        </div>
-      );
-    }
-  }
-  
-  export default VideoComponent;
+		// if current time outside of range, restart loop
+		if (this.state.player.getCurrentTime() >= this.state.range[1]) {
+			this.state.player.seekTo(this.state.range[0]);
+		}
+	}
+
+	/**
+	 * When youtube video is ready
+	 *
+	 * @param {*} event
+	 * @memberof VideoComponent
+	 */
+	onReady(event) {
+		console.log(`YouTube Player object for videoId: "${this.state.videoId}" has been saved to state.`);
+		// sets player object, and other info
+		this.setState({
+			player: event.target,
+			progress: 0,
+			duration: event.target.getDuration(),
+		});
+	}
+
+	/**
+	 * sets speed of video via player api
+	 *
+	 * @memberof VideoComponent
+	 */
+	setSpeed() {
+		this.state.player.setPlaybackRate(0.5);
+	}
+
+	/**
+	 * Plays video, and starts interval timer
+	 *
+	 * @memberof VideoComponent
+	 */
+	playVideo() {
+		this.state.player.playVideo();
+		// start timer
+		this.videoTimer = setInterval(this.videoTimerProgress, 100);
+	}
+
+	/**
+	 * Pauses video, and clears interval
+	 *
+	 * @memberof VideoComponent
+	 */
+	pauseVideo() {
+		this.state.player.pauseVideo();
+		clearInterval(this.videoTimer);
+	}
+
+	/**
+	 * Handles data returned from range track
+	 *
+	 * @param {*} event
+	 * @memberof VideoComponent
+	 */
+	rangeHandler(event) {
+		console.log('rangeHandler', event);
+		// sets range in state
+		this.setState({
+			range: event,
+		});
+		// resets player to start of range
+		this.state.player.seekTo(event[0]);
+		// play video
+		this.playVideo();
+	}
+
+	/**
+	 * render component
+	 *
+	 * @returns
+	 * @memberof VideoComponent
+	 */
+	render() {
+		return (
+			<div class="wrapper">
+				<section class="main">
+					<YouTube
+						videoId={this.state.videoId}
+						onReady={this.onReady}
+						opts={opts}
+						containerClassName="video"
+					/>
+					<div class="controls">
+						<div class="track-wrapper">
+							<div
+								class="track-progress"
+								style={{ width: this.state.progress + '%' }}
+							></div>
+						</div>
+						<TrackRange rangeHandler={this.rangeHandler} duration={this.state.duration} />
+						<div class="buttons">
+							<button onClick={this.playVideo}>Play</button>
+							<button onClick={this.pauseVideo}>Pause</button>
+							<button onClick={this.setSpeed}>Slow</button>
+						</div>
+					</div>
+				</section>
+				<section class="nav">YOOO</section>
+			</div>
+		);
+	}
+}
+
+export default VideoComponent;
