2fd1857c279af1055b93a7609e11b5f6cb574890
Chris <5120660+poppahorse@users.noreply.github.com>
2020-01-31 16:51:18 +0000

    feat: added video import
    refactored videos-list into a class

--- src/modules/videos/videos-list-component.js
+++ src/modules/videos/videos-list-component.js
@@ -1,44 +1,131 @@
 import React from 'react';
 import VideoListItemComponent from './video-list-item-component';
 import './Videos.css';
+import store from 'store';
+import queryString from 'query-string';
+
+const videosMock = [
+  {
+    "title": "The Jimi Hendrix Experience - Purple Haze (Audio)",
+    "id": "WGoDaYjdfSg"
+  },
+  {
+    "title": "Metallica: Nothing Else Matters (Official Music Video)",
+    "id": "tAGnKpE4NCI"
+  },
+  {
+    "title": "Metallica-Master Of Puppets (Lyrics)",
+    "id": "xnKhsTXoKCI"
+  },
+  {
+    "title": "The Troggs- Wild Thing",
+    "id": "4qHX493bB3U"
+  },
+  {
+    "title": "Oasis - Songbird (Official Video)",
+    "id": "0KJgBkreAuw"
+  }
+];
+
+class VideosListComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+		//value: 'ZZYWbx8mqZc',
+		//value: '8R6StQfLNbw',
+		//value: 'ogl16QSw6aY',
+		value: 'https://www.youtube.com/watch?v=PewShF3gNG4',
+		videos: store.get('videos') || videosMock
+    };
+
+    store.set('videos', this.state.videos);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    //alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+	var that = this;
+	
+	// parse url
+	var ytUrl = queryString.parseUrl(this.state.value);
+	console.log('url', ytUrl.query.v);
+
+	// TODO check if already exists
+
+	if(!ytUrl.query.v) {
+		alert('Not valid youtube URL, try like https://www.youtube.com/watch?v=PewShF3gNG4');
+	}
+	else {
+
+	// run youtube api request
+    var API_key = "AIzaSyDN3VqrUy4NsJv9D-3yR9pYgE9WRxS4LMA";
+    var maxResults = 10;
+    var url =
+      "https://www.googleapis.com/youtube/v3/videos?key=" +
+      API_key +
+      "&id=" +
+      ytUrl.query.v +
+      "&part=snippet,id&order=date&maxResults=" +
+	  maxResults;
 
 
-function VideosListComponent() {
+    fetch(url)
+      .then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        console.log(data.items[0].snippet.title)
+        
+        that.setState(state => {
+          const videos = [...state.videos, {
+            "title": data.items[0].snippet.title,
+            "id": state.value,
+          }];
+    
+          store.set('videos', videos);
+          
+          return {
+            videos: videos,
+            value: '',
+          };
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    
+	}
+  }
 
-  const videos = [
-    {
-      "url": "https://www.youtube.com/watch?v=WGoDaYjdfSg",
-      "title": "The Jimi Hendrix Experience - Purple Haze (Audio)",
-      "img": "https://i.ytimg.com/vi/WGoDaYjdfSg/hqdefault.jpg",
-      "id": "WGoDaYjdfSg"
-    },
-    {
-      "url": "https://www.youtube.com/watch?v=tAGnKpE4NCI",
-      "title": "Metallica: Nothing Else Matters (Official Music Video)",
-      "img": "https://i.ytimg.com/vi/tAGnKpE4NCI/hqdefault.jpg",
-      "id": "tAGnKpE4NCI"
-    },
-    {
-      "url": "https://www.youtube.com/watch?v=xnKhsTXoKCI",
-      "title": "Metallica-Master Of Puppets (Lyrics)",
-      "img": "https://i.ytimg.com/vi/xnKhsTXoKCI/hqdefault.jpg",
-      "id": "xnKhsTXoKCI"
-    },
-    {
-      "url": "https://www.youtube.com/watch?v=4qHX493bB3U",
-      "title": "The Troggs- Wild Thing",
-      "img": "https://i.ytimg.com/vi/4qHX493bB3U/hqdefault.jpg",
-      "id": "4qHX493bB3U"
-    }
-  ];
-
-  return (
-    <ul>
-        {videos.map(function(video, index) {
-					return <VideoListItemComponent key={index} video={video} />;
-				})}
-    </ul>
-  );
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Import:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <ul>
+            {this.state.videos.map(function(video, index) {
+              return <VideoListItemComponent key={index} video={video} />;
+            })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default VideosListComponent;
