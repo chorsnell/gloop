9d4329a555532f95c096ceaf6ff38aec55b9c54d
Chris <5120660+poppahorse@users.noreply.github.com>
2020-01-31 16:53:11 +0000

    feature: allow saved tracks on each video

--- src/modules/videos/video/video-component.js
+++ src/modules/videos/video/video-component.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './video.css';
 import YouTube from 'react-youtube';
 import TrackRange from '../../track/track-range';
+var store = require('store');
+// Example plugin usage:
+//var operationsPlugin = require('store/plugins/operations');
+//store.addPlugin(operationsPlugin)
 
 // youtube video options
 const opts = {
@@ -29,13 +33,23 @@ class VideoComponent extends React.Component {
 	constructor(props) {
 		super(props);
 
+		var videos = store.get('videos') || [];
+		const video = videos.find((v) => v.id === props.match.params.id);
+		const index = videos.findIndex((v) => v.id === props.match.params.id);
+		console.log('main', video, index);
+
 		this.state = {
 			videoId: props.match.params.id,
 			player: null,
 			progress: 0,
 			duration: 100,
 			range: [0, 100],
+			tracks: video.tracks || [],
+			video: video,
+			index: index,
 		};
+
+		console.log(this.state.video);
 
 		this.onReady = this.onReady.bind(this);
 		this.playVideo = this.playVideo.bind(this);
@@ -43,8 +57,35 @@ class VideoComponent extends React.Component {
 		this.setSpeed = this.setSpeed.bind(this);
 		this.videoTimerProgress = this.videoTimerProgress.bind(this);
 		this.rangeHandler = this.rangeHandler.bind(this);
+		this.saveTrack = this.saveTrack.bind(this);
 
 		this.videoTimer = null;
+
+		
+		this.TrackRangeElement = React.createRef();
+	}
+
+	saveTrack() {
+		console.log('save', this.state.range);
+		
+		//this.state.video.tracks.push(this.state.range);
+		//store.set('videos[6]', this.state.video);
+		console.log(this.state.video);
+
+		this.setState(state => {
+			const tracks = [...state.video.tracks, this.state.range];
+			const video = state.video;
+			video.tracks = tracks;
+
+			var videos = store.get('videos') || [];
+			videos[this.state.index] = video;
+	  
+			store.set('videos', videos);
+			
+			return {
+				tracks: tracks,
+			};
+		  });
 	}
 
 	/**
@@ -119,6 +160,7 @@ class VideoComponent extends React.Component {
 	 * @memberof VideoComponent
 	 */
 	rangeHandler(event) {
+		this.TrackRangeElement.current.changeTrack(event);
 		console.log('rangeHandler', event);
 		// sets range in state
 		this.setState({
@@ -138,33 +180,41 @@ class VideoComponent extends React.Component {
 	 */
 	render() {
 		return (
-			<div class="wrapper">
-				<section class="main">
+			<div className="wrapper">
+				<section className="main">
 					<YouTube
 						videoId={this.state.videoId}
 						onReady={this.onReady}
 						opts={opts}
 						containerClassName="video"
 					/>
-					<div class="controls">
-						<div class="track-wrapper">
+					<div className="controls">
+						<div className="track-wrapper">
 							<div
-								class="track-progress"
+								className="track-progress"
 								style={{ width: this.state.progress + '%' }}
 							></div>
 						</div>
-						<TrackRange rangeHandler={this.rangeHandler} duration={this.state.duration} />
-						<div class="buttons">
+						<TrackRange rangeHandler={this.rangeHandler} duration={this.state.duration} ref={this.TrackRangeElement} />
+						<div className="buttons">
 							<button onClick={this.playVideo}>Play</button>
 							<button onClick={this.pauseVideo}>Pause</button>
 							-
 							<button onClick={() => this.setSpeed(0.5)}>0.5</button>
 							<button onClick={() => this.setSpeed(0.75)}>0.75</button>
 							<button onClick={() => this.setSpeed(1)}>1</button>
+							-
+							<button onClick={this.saveTrack}>Save</button>
 						</div>
 					</div>
 				</section>
-				<section class="nav">YOOO</section>
+				<section className="nav">
+				<ul>
+					{this.state.tracks.map((track, index) =>
+						<li key={index}>{track[0]} - {track[1]} - name - edit - <button onClick={() => this.rangeHandler(track)}>use</button></li>
+					)}
+				</ul>
+				</section>
 			</div>
 		);
 	}
