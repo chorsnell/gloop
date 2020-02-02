da780824bc34d50b6c153ee94c53fe9492107f10
Chris <5120660+poppahorse@users.noreply.github.com>
2020-02-02 11:47:44 +0000

    feature: show number of tracks on video list

--- src/modules/videos/videos-list-item-component.js
+++ src/modules/videos/videos-list-item-component.js
@@ -6,7 +6,7 @@ function VideoListItemComponent({ video }) {
     <li key={video.id}>
         <a href={ '/video/' + video.id }>
             <img src={ 'https://i.ytimg.com/vi/'+video.id+'/hqdefault.jpg' } />
-            <div>{video.title}</div>
+            <div>({video.tracks.length}) {video.title}</div>
         </a>
     </li>
   );
