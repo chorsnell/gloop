c5605321528cec6f72627b601d10ca611bd504b1
Chris <5120660+poppahorse@users.noreply.github.com>
2020-02-16 17:56:31 +0000

    feature: add login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+import React, { Component  } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import VideosListComponent from './modules/videos/videos-list-component';
 import VideoComponent from './modules/videos/video/video-component';
 
-function App() {
-  return (
-		<Router>
+// https://medium.com/firebase-developers/how-to-setup-firebase-authentication-with-react-in-5-minutes-maybe-10-bb8bb53e8834
+import firebaseApp from './firebase';
+
+class App extends Component {
+	render() {
+	  const {
+		user,
+		signOut,
+		signInWithGoogle,
+	  } = this.props;
+  
+	  return (
+		<div className="App">
+		  <header className="App-header">
+			{
+			  user
+				? <p>Hello, {user.displayName} {user.uid}}</p>
+				: <p>Please sign in.</p>
+			}
+  
+			{
+			  user
+				? <button onClick={signOut}>Sign out</button>
+				: <button onClick={signInWithGoogle}>Sign in with Google</button>
+			}
+		  </header>
+
+		  <Router>
 			<div className="wrapper">
 				<Switch>
 					<Route exact path="/">
@@ -16,7 +46,18 @@ function App() {
 				</Switch>
 			</div>
 		</Router>
-  );
-}
+		</div>
+	  );
+	}
+  }
 
-export default App;
+
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+export default withFirebaseAuth({
+	providers,
+	firebaseAppAuth,
+  })(App);
